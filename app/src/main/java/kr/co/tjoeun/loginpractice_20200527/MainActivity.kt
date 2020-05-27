c0f655dc84f116e22d35f5841e37e497aed67b90
cho881020 <kj_cho@idealidea.co.kr>
2020-05-27 20:00:49 +0900

    버튼 누르면 => 입력된 내용으로 버튼 문구 변경

--- app/src/main/java/kr/co/tjoeun/loginpractice_20200527/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/loginpractice_20200527/MainActivity.kt
@@ -19,6 +19,10 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this, "버튼 클릭 됨", Toast.LENGTH_SHORT).show()
 
+            val inputContent = contentEdt.text.toString()
+
+            eventBtn.text = inputContent
+
         }
 
     }
